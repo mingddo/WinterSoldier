a029bfeda369187b1accde54bad89d03f397dad8
zngsm <qbw0088@gmail.com>
2020-12-06 17:33:21 +0900

    [수민] feat: 회원가입 + 로그인 기본 창 생성

---
+++ src/views/Accounts/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>
+      로그인!
+    </h1>
+    <div>
+      <div>
+        <label for="username">username </label>
+        <input type="text" name="username" id="username" ut="">
+      </div>
+      <div>
+        <label for="password">password</label>
+        <input type="password" name="password" id="password" ut="">
+      </div>
+      <button>로그인</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+}
+</script>
+
+<style>
+
+</style>
