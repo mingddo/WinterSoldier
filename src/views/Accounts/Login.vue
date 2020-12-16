7c12dbecb4a26d6faac7dea9baec8770fc40d591
zngsm <qbw0088@gmail.com>
2020-12-16 22:26:16 +0900

    [수민] feat: 로그인 기능 axios 요청까지만

--- src/views/Accounts/Login.vue
+++ src/views/Accounts/Login.vue
@@ -6,11 +6,11 @@
     <div>
       <div>
         <label for="username">username </label>
-        <input type="text" name="username" id="username" ut="">
+        <input type="text" name="username" id="username" v-model="user.username">
       </div>
       <div>
         <label for="password">password</label>
-        <input type="password" name="password" id="password" ut="">
+        <input @keyup.enter="onLogin" type="password" name="password" id="password" v-model="user.password">
       </div>
       <button>로그인</button>
     </div>
@@ -18,8 +18,35 @@
 </template>
 
 <script>
+import { userlogin } from "@/api/accounts.js";
+
 export default {
   name: 'Login',
+  data () {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    }
+  },
+  methods: {
+    onLogin () {
+      console.log(this.user)
+      userlogin (this.user,
+      (res) => {
+        this.$store.commit("setIsLogined", true);
+        console.log(res)
+        localStorage.setItem('jwt', res.data.token)
+        alert('로그인이 완료되었습니다!')
+      },
+      (err) => {
+        console.log(err)
+        alert('아이디 또는 비밀번호가 일치하지 않습니다.')
+      }
+      )
+    },
+  },
 }
 </script>
 
