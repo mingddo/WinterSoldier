be9a1a8f1c0d473206af70850c893d8faf2ea871
zngsm <qbw0088@gmail.com>
2020-12-16 22:22:46 +0900

    [수민] fix: 컴포넌트 분리 / vuex 모듈화

---
+++ src/components/common/navi.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      안녕하세요!
+    </div>
+    <div>
+      누구세요;
+    </div>
+    <button>
+      로그아웃
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Navi',
+
+}
+</script>
+
+<style>
+
+</style>
