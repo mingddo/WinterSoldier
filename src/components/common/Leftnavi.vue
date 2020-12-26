b4639a4644d9f285c3d2160fd03eebda7ea58b96
zngsm <qbw0088@gmail.com>
2020-12-26 20:25:26 +0900

    [수민] fix: navbar 로그인/아웃시 문구 변경 / leftnav 바 컴포넌트 생성

---
+++ src/components/common/Leftnavi.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- <ul class="navbarCategory"> -->
+    <div>
+      <router-link to="/">Home</router-link>
+    </div>
+    <div>
+      <router-link to="/about">About</router-link>
+    </div>
+    <div>
+      <router-link to="/signup">Signup</router-link>
+    </div>
+    <div>
+      <router-link to="/login">Login</router-link>
+    </div>
+    <div>
+      <router-link to="/weather">whether api test</router-link>
+    </div>
+    <div>
+      <router-link to="/stock">stock</router-link>
+    </div>     
+      <!-- <li>주식</li>
+      <li>영화예매</li>
+      <li>날씨</li>
+      <li>로그아웃</li> -->
+    <!-- </ul> -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
