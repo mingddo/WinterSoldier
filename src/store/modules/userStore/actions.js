655e10181e00280d93bfa78b137ff81381908475
Jinwoo <phoenix9373@naver.com>
2020-12-26 20:24:18 +0900

    [진우] 저장했을 때, 세미콜론 붙는 것 때문에 커밋함.

--- src/store/modules/userStore/actions.js
+++ src/store/modules/userStore/actions.js
@@ -1,13 +1,13 @@
-import jwt_decode from "jwt-decode";
-import { findById } from "@/api/accounts.js";
+import jwt_decode from 'jwt-decode';
+import { findById } from '@/api/accounts.js';
 
-export const GET_MEMBER_INFO = async ({ commit }, token ) => {
+export const GET_MEMBER_INFO = async ({ commit }, token) => {
   let decode = jwt_decode(token);
   await findById(
     decode.username,
     (res) => {
       console.log('유저정보', res);
-      commit("setUserInfo", res.data);
+      commit('setUserInfo', res.data);
     },
     (err) => {
       console.log(err);
@@ -15,6 +15,6 @@ export const GET_MEMBER_INFO = async ({ commit }, token ) => {
   );
 };
 export const LOGOUT = ({ commit }) => {
-  commit("logout");
+  commit('logout');
   localStorage.removeItem('jwt');
-}
+};
