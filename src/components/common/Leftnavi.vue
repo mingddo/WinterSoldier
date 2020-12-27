a7fd77f0c4218c33440b8cf8f0f87a7addbc39ae
Jinwoo <phoenix9373@naver.com>
2020-12-27 15:26:02 +0900

    [진우] modify navbar for 수민

--- src/components/common/Leftnavi.vue
+++ src/components/common/Leftnavi.vue
@@ -1,38 +1,34 @@
 <template>
   <div>
-    <!-- <ul class="navbarCategory"> -->
-    <div>
-      <router-link to="/">Home</router-link>
-    </div>
-    <div>
-      <router-link to="/about">About</router-link>
-    </div>
-    <div>
-      <router-link to="/signup">Signup</router-link>
-    </div>
-    <div>
-      <router-link to="/login">Login</router-link>
-    </div>
-    <div>
-      <router-link to="/weather">whether api test</router-link>
-    </div>
-    <div>
-      <router-link to="/stock">stock</router-link>
-    </div>     
+    <ul class="navbarCategory">
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <router-link to="/signup">Signup</router-link>
+      </li>
+      <li>
+        <router-link to="/login">Login</router-link>
+      </li>
+      <li>
+        <router-link to="/weather">whether api test</router-link>
+      </li>
+      <li>
+        <router-link to="/stock">stock</router-link>
+      </li>
       <!-- <li>주식</li>
       <li>영화예매</li>
       <li>날씨</li>
       <li>로그아웃</li> -->
-    <!-- </ul> -->
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
