0b5eaf7810ac475efc5e94e155ecff6e45f4f934
zngsm <68373473+zngsm@users.noreply.github.com>
2021-01-04 01:12:41 +0900

    [수민] feat: 그룹란 생성

---
+++ src/components/Profile/UserInvite.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>날 초대한 그룹! </h1>
+    {{ invite }}
+    <button>yes</button>
+    <button>no</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    invite: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
