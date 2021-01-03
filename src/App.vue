fea206aaeee8d0561e54ddff9803ed7da80fbd75
moong3871 <moong3871@gmail.com>
2021-01-03 12:14:32 +0900

    [문정] fix: 알림 (바뀐 back에 맞춰서)

--- src/App.vue
+++ src/App.vue
@@ -21,23 +21,23 @@ export default {
   data() {
     return {
       today_todos: [],
-      today_last_todos: [], // 시간이 지난 todo
-      todos: [],
+      today_notAlarm_todos: [], // 시간이 지난 todo
+      today_alarm_todos: [],
       cnt: 0
     };
   },
   methods: {
     checkAlarm() {
-      if (this.today_last_todos.length > 0) {
+      if (this.today_notAlarm_todos.length > 0) {
         if (Notification.permission === "granted") {
               let img = 'https://i.annihil.us/u/prod/marvel/i/mg/6/00/5c802b62bc572/clean.jpg'
               var i;
-              let today_last_todos_title = ''
-              for (i=0; i < this.today_last_todos.length; i++) {
-                today_last_todos_title += this.today_last_todos[i].title + ', '
+              let today_notAlarm_todos_title = ''
+              for (i=0; i < this.today_notAlarm_todos.length; i++) {
+                today_notAlarm_todos_title += this.today_notAlarm_todos[i].title + ', '
                 this.today_todos[i].completed = "yes" // 알람띄운 todo는 completed 변경
                 // db의 todo completed여부도 변경
-                todoCompleted ({...this.today_last_todos[i], completed: "yes"},
+                todoCompleted ({...this.today_notAlarm_todos[i], completed: "yes"},
                 () => {
                   console.log('지나간 알림 completed 수정 성공')
                 },
@@ -45,8 +45,8 @@ export default {
                   console.log(err)
                 });
               }
-              let text = today_last_todos_title
-              new Notification(this.today_last_todos.length + '개의 시간 지난 할 일', { body: text, icon: img }) 
+              let text = today_notAlarm_todos_title
+              new Notification(this.today_notAlarm_todos.length + '개의 시간 지난 할 일', { body: text, icon: img }) 
         } else {
           alert('알림시간이 지난 할 일이' + this.today_last_todos.length +'개 있어요!')
         }
@@ -56,21 +56,21 @@ export default {
         let cur_hour = day.getHours();
         let cur_minute = day.getMinutes();
         var i;
-        for (i=0; i < this.today_todos.length; i++) {
-          console.log('i랑 today_todos완료여부', i, this.today_todos[i].completed)
-          if(this.today_todos[i].alarm_hour === cur_hour && this.today_todos[i].alarm_min === cur_minute && this.today_todos[i].completed === "no"){
+        for (i=0; i < this.today_alarm_todos.length; i++) {
+          console.log('i랑 today_alarm_todos완료여부', i, this.today_alarm_todos[i].completed)
+          if(this.today_alarm_todos[i].alarm_hour === cur_hour && this.today_alarm_todos[i].alarm_min === cur_minute && this.today_alarm_todos[i].completed === "no"){
             // 지원 여부 확인 및 알람 띄우기
             if (!("Notification" in window)) {
                   alert("Notification을 지원하지 않는 브라우저입니다.")
                 }
             else if (Notification.permission === "granted") {
               let img = 'https://i.annihil.us/u/prod/marvel/i/mg/6/00/5c802b62bc572/clean.jpg'
-              let text = '지금' + this.today_todos[i].title + ' 할 시간.'
+              let text =  this.today_alarm_todos[i].title + ' 할 시간.'
               new Notification('할 일', { body: text, icon: img }) // 알람
             
-              this.today_todos[i].completed = "yes" // 알람띄운 todo는 completed 변경
+              this.today_alarm_todos[i].completed = "yes" // 알람띄운 todo는 completed 변경
               // db의 todo completed여부도 변경
-              todoCompleted ({...this.today_todos[i], completed: "yes"},
+              todoCompleted ({...this.today_alarm_todos[i], completed: "yes"},
               () => {
                 console.log('completed 수정 성공')
               },
@@ -100,30 +100,47 @@ export default {
       let cur_year = day.getFullYear();
       let cur_month = day.getMonth()+ 1;
       let cur_day = day.getDate();
-      let login_hour = day.getHours();
-      let login_minute = day.getMinutes();
+      let cur_hour = day.getHours();
+      let cur_minute = day.getMinutes();
+      let today = cur_year.toString() + cur_month.toString() + cur_day.toString();
+      // let nowTime = cur_hour.toString() + cur_minute.toString()
+
 
       todoList (
-      (res) => {
-        var i;
-        for (i=0; i < res.data.length; i++){ // 내가 쓴 todo만 가져오기
-          if (res.data[i].user == this.userInfo.id) {
-            this.todos.push(res.data[i])
-            // console.log('todo', this.todo)
-          }
-        }
-        var j;
-        for (j=0; j < this.todos.length; j++){ // 오늘 날짜에 해당하는 todo만 today_todos에 넣기
-          if ((this.todos[j].alarm_year === cur_year) && (this.todos[j].alarm_month === cur_month) && (this.todos[j].alarm_date === cur_day)) {
-            if ((this.todos[j].completed === "no") && (this.todos[j].alarm_hour === login_hour) && (this.todos[j].alarm_min < login_minute)) {
-              this.today_last_todos.push(this.todos[j]);
-            } else if ((this.todos[j].completed === "no") && (this.todos[j].alarm_hour < login_hour)) {
-              this.today_last_todos.push(this.todos[j]);
+        (res) => {
+          this.today_todos = res.data.todolist[today]
+          var i;
+          for (i=0; i < this.today_todos.length; i++){
+            if ((this.today_todos[i].completed === "no") && (this.today_todos[i].alarm_hour === cur_hour) && (this.today_todos[i].alarm_min < cur_minute)) {
+              this.today_notAlarm_todos.push(this.today_todos[i]);
+            } else if ((this.today_todos[i].completed === "no") && (this.today_todos[i].alarm_hour < cur_hour)) {
+              this.today_notAlarm_todos.push(this.today_todos[i]);
             } else {
-              this.today_todos.push(this.todos[j]);
+              this.today_alarm_todos.push(this.today_todos[i]);
             }
           }
-        }
+
+          
+
+        // var i;
+        // for (i=0; i < res.data.length; i++){ // 내가 쓴 todo만 가져오기
+        //   if (res.data[i].user == this.userInfo.id) {
+        //     this.todos.push(res.data[i])
+        //     // console.log('todo', this.todo)
+        //   }
+        // }
+        // var j;
+        // for (j=0; j < this.todos.length; j++){ // 오늘 날짜에 해당하는 todo만 today_todos에 넣기
+        //   if ((this.todos[j].alarm_year === cur_year) && (this.todos[j].alarm_month === cur_month) && (this.todos[j].alarm_date === cur_day)) {
+        //     if ((this.todos[j].completed === "no") && (this.todos[j].alarm_hour === login_hour) && (this.todos[j].alarm_min < login_minute)) {
+        //       this.today_last_todos.push(this.todos[j]);
+        //     } else if ((this.todos[j].completed === "no") && (this.todos[j].alarm_hour < login_hour)) {
+        //       this.today_last_todos.push(this.todos[j]);
+        //     } else {
+        //       this.today_todos.push(this.todos[j]);
+        //     }
+        //   }
+        // }
         this.checkAlarm()
       },
       (err) => {
